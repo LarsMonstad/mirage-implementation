01a27bd6df0cf0fa9d253a5da550d3ed9709530a
Lars Løberg Monstad <lars.monstad@gmail.com>
2022-10-16 06:45:22 +0200

    Update 4020bigmemUnix.sh

--- JobScriptsSigma2/4020bigmemUnix.sh
+++ JobScriptsSigma2/4020bigmemUnix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --account=nn9750k --job-name=bigmemtraining4020
-#SBATCH --time=1-0:0:0
+#SBATCH --time=3-0:0:0
 #SBATCH --ntasks=1 --cpus-per-task=40
 #SBATCH --mem-per-cpu=20G
 #SBATCH -e training4020.e
